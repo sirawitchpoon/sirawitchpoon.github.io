b594fd900540ac7d304ea381bc08eed1da491e1a
Sirawitch <sirawitchbu@kkumail.com>
2023-08-04 21:00:54 +0700

    publish lab5p2

---
+++ lab5p2/scripts/P2-script.js
@@ -0,0 +1,28 @@
+function readInput() {
+    let list = [];
+    while (true) {
+      let input = prompt("Enter a positive integer:");
+      let num = parseInt(input);
+      if (isNaN(num)) {
+        continue;
+      } else if (num < 0) {
+        return list;
+      } else {
+        list.push(num);
+      }
+    }
+  }
+  
+  function displayStats(list) {
+    let sum = 0;
+    list.forEach(function(list) {
+      sum += list;
+    });
+    let avg = list.length > 0 ? sum / list.length : 0;
+    let min = list.length > 0 ? Math.min(...list) : 0;
+    let max = list.length > 0 ? Math.max(...list) : 0;
+    alert("For the list " + list + " the average is " + avg + ", the mininum is " + min + ", and the maximum is " + max)
+  }
+
+list = readInput();
+displayStats(list);
